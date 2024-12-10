f6155433bde9237e48b3cf7704fe8a6eda16bbe8
ZhaoYiwei <yzha1011@student.monash.edu>
2024-12-10 20:55:49 +0800

    complete the date part

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,31 +1,41 @@
 <template>
   <div class="city">
-    <van-search 
+    <div class="top">
+      <van-search 
       v-model="searchValue" 
       placeholder="城市/区域/位置"
       show-action
       action-text="取消"
       shape="round"
       @cancel="cancelClick"
-    />
-    <van-tabs v-model:active="tabActive" color="#ff9854">
-      <!-- <van-tab title="国内·港澳台"></van-tab>
-      <van-tab title="海外"></van-tab> -->
-      <template v-for="(value, key, index) in allCities" :key="key">
-        <van-tab :title="value.title"></van-tab>
-      </template>
-    </van-tabs>
-
+      />
+      <van-tabs v-model:active="tabActive" color="#ff9854">
+        <!-- <van-tab title="国内·港澳台"></van-tab>
+        <van-tab title="海外"></van-tab> -->
+        <template v-for="(value, key, index) in allCities" :key="key">
+          <van-tab :title="value.title" :name="key"></van-tab>
+        </template>
+      </van-tabs>
+    </div>
+    <div class="content">
+      <!-- <city-group :group-data="currentGroup"/> -->
+       <template v-for="(value, key, index) in allCities">
+        <!-- <h2 v-show="tabActive === key">{{ value.title }}</h2> -->
+        <city-group v-show="tabActive === key" :group-data="value" />
+       </template>
+    </div>
   </div>
 </template>
 
 <script setup>
 // import { getCityAll } from '@/services';
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router';
 import useCityStore from '@/stores/modules/city';
 import { storeToRefs } from 'pinia';
 // import ywRequest from "@/services/request"
+
+import cityGroup from './cpns/city-group.vue';
 
 const router = useRouter()
 const searchValue = ref("")
@@ -39,13 +49,20 @@ const cityStore = useCityStore()
 cityStore.fetchAllCitiesData()
 const { allCities } = storeToRefs(cityStore)
 
-// const allCity = ref({})
-// getCityAll().then(res => {
-//   allCity.value = res.data
-// })
+const currentGroup = computed(() => allCities.value[tabActive.value])
 
 </script>
 
 <style scoped>
+.city {
+  .top {
+    position: relative;
+    z-index: 9;
+  }
 
+  .content {
+    height: calc(100vh - 98px);
+    overflow-y: auto;
+  }
+}
 </style>
--- src/views/home/cpns/home-search-box.vue
+++ src/views/home/cpns/home-search-box.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="search">
+  <div class="search-box">
     <div class="location">
-      <div class="city" @click="cityClick">广州</div>
+      <div class="city" @click="cityClick">{{ currentCity.cityName }}</div>
       <div class="position" @click="positionClick">
         <span class="text">我的位置</span>
         <img src="@/assets/img/home/icon_location.png" alt="">
       </div>
     </div>
+
+    <div class="section date-range" @click="showCalendar = true">
+      <div class="start">
+        <div class="date">
+          <span class="tip">入住</span>
+          <span class="time">{{ startDate }}</span>
+        </div>
+        <div class="stay">共{{ stayCount }}晚</div>
+      </div>
+      <div class="end">
+        <div class="date">
+          <span class="tip">离店</span>
+          <span class="time">{{ endDate }}</span>
+        </div>
+      </div>
+    </div>
+    <van-calendar 
+      v-model:show="showCalendar" 
+      type="range" 
+      @confirm="onConfirm" 
+      color="#ff9854"
+      :show-confirm = "false"
+      :round="false"
+      />
   </div>
 </template>
 
 <script setup>
+import useCityStore from '@/stores/modules/city';
+import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue'
+import { formatMonthDay, getDiffDays } from '@/utils/format_date';
 
 const router = useRouter()
 
@@ -31,10 +59,32 @@ const positionClick = () => {
   })
 }
 
+const cityStore = useCityStore()
+const { currentCity } =storeToRefs(cityStore)
 
+const nowDate = new Date()
+const startDate = ref(formatMonthDay(nowDate))
+const newDate = new Date()
+newDate.setDate(nowDate.getDate() + 1)
+const endDate = ref(formatMonthDay(newDate))
+const stayCount = ref(getDiffDays(nowDate, newDate))
+
+const showCalendar = ref(false)
+const onConfirm = (value) => {
+  const selectStartDate = value[0]
+  const selectEndDate = value[1]
+  startDate.value = formatMonthDay(selectStartDate)
+  endDate.value = formatMonthDay(selectEndDate)
+  stayCount.value = getDiffDays(selectStartDate, selectEndDate)
+  showCalendar.value = false
+}
 </script>
 
 <style lang="less" scoped>
+.search-box {
+  --van-calendar-popup-height: 100%
+}
+
 .location {
   display: flex;
   align-items: center;
@@ -64,6 +114,54 @@ const positionClick = () => {
       width: 18px;
       height: 18px;
     }
+  }
+}
+
+.section {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 0 20px;
+  color: #999;
+
+  .start {
+    flex: 1;
+    display: flex;
+    height: 44px;
+    align-items: center;
+  }
+
+  .end {
+    min-width: 30%;
+    padding-left: 20px;
+  }
+
+  .date {
+    display: flex;
+    flex-direction: column;
+
+    .tip {
+      font-size: 12px;
+      color: #999;
+    }
+
+    .time {
+      margin-top: 3px;
+      color: #333;
+      font-size: 15px;
+      font-weight: 500;
+    }
+  }
+}
+
+.date-range {
+  height: 44px;
+
+  .stay {
+    flex: 1;
+    text-align: center;
+    font-size: 12px;
+    color: #666;
   }
 }
 </style>
