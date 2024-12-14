061a0704702e95ebd709d388618750f2e7d9e7bd
ZhaoYiwei <yzha1011@student.monash.edu>
2024-12-14 17:24:10 +0800

    add loading component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import tabBar from '@/components/tab-bar/tab-bar.vue';
 import { useRoute } from 'vue-router';
+import loading from './components/loading/loading.vue';
 
 const route = useRoute()
 </script>
@@ -8,11 +9,8 @@ const route = useRoute()
 <template>
   <div class="app">
     <router-view/>
-    <!-- <router-link to="/home">首页</router-link>
-    <router-link to="/favor">收藏</router-link>
-    <router-link to="/order">订单</router-link>
-    <router-link to="/message">消息</router-link> -->
     <tab-bar v-if="!route.meta.hideTabBar"/>
+    <loading />
   </div>
 
 </template>
